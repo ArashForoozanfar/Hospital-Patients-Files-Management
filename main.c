c816a309637c79794261514c16f17495d308677d
Arash <foroozanfararash@gmail.com>
2020-06-04 23:49:30 +0430

    first change
    Co-Authored-By: null <ArashForoozanfar@users.noreply.github.com>

--- main.c
+++ main.c
@@ -6,27 +6,27 @@ int main()
 {
   char Username[100];
   char Password[100];
-  char U[] = {"Admin"};
-  char P[] = {"Abcd1234"};
+  char U1[] = {"Admin1"}; char U2[] = {"Admin2"};
+  char P1[] = {"Abcd1234"}; char P2[] = {"Aqwe1"};
   int n;
   printf("Hi\nWelcome to Hospital system:)\n");
-  printf("Please enter 1-3 key for below states:\n");
-  printf("Insert(1)\nDelete(2)\nUpdate(3)\n");
+  printf("Please enter 1-4 key for below states:\n");
+  printf("Insert(1)\nDelete(2)\nUpdate(3)\nSerach(4)\n");
   scanf("%d", &n);
 
   if (n == 1) {
     //Insert
     puts("For adding new patients information you need to Login into system");
     puts("please enter your Username and Password:");
-    puts("Username: ");
+    printf("Username:");
     scanf("%s", Username);
-    puts("Password: ");
+    printf("Password:");
     scanf("%s", Password);
-    if (strcmp(Username, U) == 0 && strcmp(Password, P) == 0) {
+    if (strcmp(Username, U1) == 0 && strcmp(Password, P1) == 0  || strcmp(Username, U2) == 0 && strcmp(Password, P2) == 0) {
       puts("You entered succssesfully!");
     }
     else {
-      puts("Username or Password was incorrect, please try again");
+      puts("Username or Password was incorrect, please try again...");
     }
   }
 
@@ -38,4 +38,7 @@ int main()
     //Update
   }
 
+  if (n == 4) {
+    //Serach
+  }
 }
