bb29d364ee15f6a08fed42fad714d030b5968639
Arash <foroozanfararash@gmail.com>
2020-06-06 11:47:26 +0430

    imp change
    Co-Authored-By: null <ArashForoozanfar@users.noreply.github.com>

--- main.c
+++ main.c
@@ -6,25 +6,28 @@
 #include "structures.txt"
 int main()
 {
-  int q, n, m, k, l;
+  int n, m, k, l, q = 0, b = 0, cnt = 0;
   struct pInfo p;
   struct Users ad[10];
   FILE* F = fopen("PatientsInfo.txt", "a+");
   FILE* P = fopen("Admins.txt", "a+");
-  for (int i = 0; i < 10 && ad[i].Name != NULL; i++) {
-    fscanf(P, "%s", ad[i].userName);
-    fscanf(P, "%s", ad[i].password);
-    if (ad[i].Name == NULL) {
-        q = i;
+  for (int i = 0; i < 10; i++) {
+    if (ad[i].Name != NULL) {
+      q = i;
+      fscanf(P, "%s", ad[q].Name);
+      fscanf(P, "%s", ad[q].userName);
+      fscanf(P, "%s", ad[q].password);
     }
   }
+  b = q - 4;
   char Username[100];
   char Password[100];
   char User1[] = {"Admin1"};
   char Pass1[] = {"Abcd1234"};
+  char delAdmin[100];
   printf("Hello :D\nWelcome to Hospital system:)\n");
   printf("Please enter 1-4 key for below states:\n");
-  printf("Insert(1)\nDelete(2)\nUpdate(3)\nSerach(4)\n");
+  printf("Insert(1)\nDelete(2)\nUpdate(3)\nSerach(4)\nExit(0)\n");
   scanf("%d", &n);
 
   if (n == 1) {
@@ -37,7 +40,7 @@ int main()
     scanf("%s", Password);
     if (strcmp(Username, User1) == 0 && strcmp(Password, Pass1) == 0) {
       puts("You entered succssesfully as Adminastrator!");
-      puts("Please select(1) to enter patients information  or  select(2) to add new admin");
+      puts("Please select(1) to enter patients information  or  select(2) to add new admin  or  select(3) to remove admin");
       scanf("%d", &m);
       if (m == 1) {
         #include "Insert.txt"
@@ -55,7 +58,7 @@ int main()
           printf("Password: ");
           scanf("%s", ad[i].password);
           fprintf(P, "%s\n", ad[i].password);
-      }
+        }
         puts("New Admins succssesfully added to system...");
         puts("Now select(1) to enter new information or (0) to Exit...");
         scanf("%d", &k);
@@ -64,28 +67,35 @@ int main()
         } else if (k == 0) {
           exit(0);
         }
-      }
-    }
-    else {
-      puts("Username or Password was incorrect, please try again...");
+      } else if (m == 3) {
+        #include "Delete Admins.txt"
     }
   }
+    else {
+        puts("Username or Password was incorrect, please try again...");
+      }
+    }
 
   if (n == 2) {
     //Delete
     puts("To Delete patients information you need to Login to system");
     #include "Login.txt"
-  }
+    }
 
   if (n == 3) {
     //Update
     puts("To Update patients information you need to Login to system:");
     #include "Login.txt"
-  }
+    }
 
   if (n == 4) {
     //Serach
     puts("To Search patients information you need to Login to system:");
     #include "Login.txt"
+    }
+
+  if (n == 0) {
+    //exit
+    exit (0);
   }
 }
